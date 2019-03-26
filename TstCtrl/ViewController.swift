008dd4e150064351d6779e306590331fa9e2c527
Strepetov Maxim <maxim.strepetov@icloud.com>
2019-03-26 22:40:45 +0600

    Initial Commit

---
+++ TstCtrl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TstCtrl
+//
+//  Created by Максим Стрепетов on 26/03/2019.
+//  Copyright © 2019 Максим Стрепетов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
